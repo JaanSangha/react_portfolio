f3374724bb55d1b2f2ee0b34935920050a0135e8
Jaan <jaansangha10@gmail.com>
2023-09-01 17:02:57 -0400

    bug fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./Home";
 import Cardemon from "./Cardemon";
-import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
 
