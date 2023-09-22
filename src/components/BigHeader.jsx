d6d7ff1b7bc8f7b376362b82e06d50c896cd6f35
jaan <jaansangha10@gmail.com>
2023-09-22 15:38:19 -0400

    added animation to header

--- src/components/BigHeader.jsx
+++ src/components/BigHeader.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion"
 import MePic from "./img/grad.jpg"
 
 function BigHeader() {
@@ -9,12 +10,25 @@ function BigHeader() {
                     <div class="container">
                         <div class="row">
                             <div class="col-xl-5 col-lg-5 col-md-6 text-center">
-                                <img class="rounded-circle shadow-4-strong" alt="avatar2" src={MePic} />
+                                <motion.img 
+                                class="rounded-circle shadow-4-strong" alt="avatar2" src={MePic}
+                                initial={{ y: -1000 }}
+                                animate={{ y: 0 }}
+                                transition={{ type: "tween", duration:1}}
+                                />
                             </div>
+
+                            
                             <div class="col-xl-7 col-lg-7 col-md-6 my-auto">
+                            <motion.div
+                            initial={{ y: 1000 }}
+                                animate={{ y: 0 }}
+                                transition={{ type: "tween", duration:1}}>
                                 <h1 class="text-md-start">Jaan Sangha</h1>
                                 <h2 class="text-md-start">Game Programmer / Full Stack Developer</h2>
+                                </motion.div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>  
