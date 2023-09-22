963ad900e8199ff9b7f515003a456a4b8f0a2fa0
jaan <jaansangha10@gmail.com>
2023-09-22 17:37:53 -0400

    added border to main image

--- src/components/BigHeader.jsx
+++ src/components/BigHeader.jsx
@@ -10,7 +10,7 @@ function BigHeader() {
        
                             <div class="col-xl-5 col-lg-5 col-md-6 text-center my-auto">
                                 <motion.img 
-                                class="rounded-circle shadow-4-strong" alt="avatar2" src={MePic}
+                                class="rounded-circle shadow-4-strong border border-dark" alt="avatar2" src={MePic}
                                 initial={{ y: -1000 }}
                                 animate={{ y: 0 }}
                                 transition={{ type: "tween", duration:1}}
