a78e3aa93e93849c95d35516952b2a68239057d3
jaan <jaansangha10@gmail.com>
2024-01-02 18:06:06 -0500

    updated petstagram and twitter bots

--- src/components/Petstagram.jsx
+++ src/components/Petstagram.jsx
@@ -22,7 +22,7 @@ return (
             <div class="row">
                 <div class="col-lg-10 mx-auto">
                     <h3>Introduction</h3>
-                    <p class="lead mb-5">Using Javascript and HTML/CSS I created a React.js app that is basically social media for pets! The app uses MongoDB to store all the users and posts so they can be saved and retrieved when needed. The backend uses express.js, node.js and jsonwebtoken to host a server that can be interacted with by the front end. </p>
+                    <p class="lead mb-5">Using Javascript, Node and HTML/CSS I created a React.js app that is basically social media for pets! The app uses MongoDB to store all the users and posts so they can be saved and retrieved when needed. The backend uses express.js, node.js and jsonwebtoken and is hosted on Heroku and can be interacted with by the front end which is hosted on Netlify. The app can be checked out <a href="https://gleaming-blancmange-1967b0.netlify.app">here</a>. </p>
                     <h3>Logging In</h3>
                     <p class="lead mb-5">User information is saved in MongoDB Atlas and is hosted on a server. The login screen uses a form and checks the input of the email and password with thr database to see if it is a match. If it is the user is granted an authentication token and can access the rest of the website. If it is not recognized the user must sign up and input all the necessary fields then it will be pushed to the database and they can log in.</p>
                     <h3>Posts</h3>
