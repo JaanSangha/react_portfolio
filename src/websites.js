c759635c0c5cd122dfd7ffeb9a6ab2edd9a1ec20
jaan <jaansangha10@gmail.com>
2024-02-20 21:54:06 -0500

    added simaya website

--- src/websites.js
+++ src/websites.js
@@ -1,5 +1,5 @@
 import RSThumb from "./components/img/annaThumbedit.png"
-
+import SFThumb from "./components/img/simayathumb.png"
 
 const websites = [
 {
@@ -7,6 +7,12 @@ const websites = [
     title: "Rumours of Stevie",
     thumbnail: RSThumb,
     link: "https://rumoursofstevie.com/"
+},
+{
+    id: 2,
+    title: "Simaya Foundation",
+    thumbnail: SFThumb,
+    link: "https://simayafoundation.org/"
 },
 ]
 
