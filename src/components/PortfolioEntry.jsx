144e059f5fb71e3680c63e18cb59a3558946ba87
jaan <jaansangha10@gmail.com>
2024-05-10 19:47:57 -0400

    disabled card animations on mobile

--- src/components/PortfolioEntry.jsx
+++ src/components/PortfolioEntry.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 function PortfolioEntry(props) {
-    return (
-        <div class="col">
-        <Link to={props.link}>
-          <motion.div class="card shadow-sm" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} data-custom-cursor>
-            <img class="rounded" src={props.thumbnail} alt=""/>
-            <div class="card-img-overlay">
-              <h3 class="card-title">{props.title}</h3>
+  const isMobile = window.innerWidth <= 768; // Adjust the width as needed for your mobile breakpoint
+
+  return (
+    <div className="col">
+      <Link to={props.link}>
+        {isMobile ? (
+          <div className="card shadow-sm" data-custom-cursor>
+            <img className="rounded" src={props.thumbnail} alt="" />
+            <div className="card-img-overlay">
+              <h3 className="card-title">{props.title}</h3>
+            </div>
+          </div>
+        ) : (
+          <motion.div
+            className="card shadow-sm"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.8 }}
+            data-custom-cursor
+          >
+            <img className="rounded" src={props.thumbnail} alt="" />
+            <div className="card-img-overlay">
+              <h3 className="card-title">{props.title}</h3>
             </div>
           </motion.div>
-        </Link>
-        </div>
-    );
+        )}
+      </Link>
+    </div>
+  );
 }
 
 export default PortfolioEntry;
