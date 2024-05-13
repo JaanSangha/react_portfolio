10139b6217f713e8b12b6e3cd5adc8e88b8e72a6
jaan <jaansangha10@gmail.com>
2024-05-13 00:00:00 -0400

    added more comics

--- src/components/Comic.jsx
+++ src/components/Comic.jsx
@@ -12,10 +12,19 @@ import comic10 from "./img/comics/Snoopy10.jpg";
 import comic11 from "./img/comics/Snoopy11.jpg";
 import comic12 from "./img/comics/Snoopy12.jpg";
 import comic13 from "./img/comics/Snoopy13.jpg";
+import comic14 from "./img/comics/Snoopy14.jpg";
+import comic15 from "./img/comics/Snoopy15.jpg";
+import comic16 from "./img/comics/Snoopy16.jpg";
+import comic17 from "./img/comics/Snoopy17.jpg";
+import comic18 from "./img/comics/Snoopy18.jpg";
+import comic19 from "./img/comics/Snoopy19.jpg";
+import comic20 from "./img/comics/Snoopy20.jpg";
+import comic21 from "./img/comics/Snoopy21.jpg";
+import comic22 from "./img/comics/Snoopy22.jpg";
 
 function Comic() {
   // List of comic images
-  const comicImages = [comic1, comic2, comic3, comic4, comic5, comic6, comic7, comic8, comic9, comic10, comic11, comic12, comic13];
+  const comicImages = [comic1, comic2, comic3, comic4, comic5, comic6, comic7, comic8, comic9, comic10, comic11, comic12, comic13, comic14, comic15, comic16, comic17, comic18, comic19, comic20, comic21, comic22,  ];
 
   // State to hold the selected comic image
   const [selectedComic, setSelectedComic] = useState(null);
