927e0c4923257f0d227ee26226bece3d9ffab83a
jaan <jaansangha10@gmail.com>
2024-07-08 20:51:41 -0400

    changed small header

--- src/components/SmallHeader.jsx
+++ src/components/SmallHeader.jsx
@@ -1,15 +1,44 @@
 import React from "react";
+import Logo from "./img/j.png";
+import "../styles.css";
 
 function SmallHeader(props) {
-    return (
-        <div className="headersmall">
-            <nav aria-label="breadcrumb">
-            <ol class="breadcrumb p-3 rounded-3">
-            <li class="breadcrumb-item" data-custom-cursor><a href="/">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{props.name}</li>
-            </ol>
-            </nav>
+    return (  
+        <nav className="smallnav navbar-expand-md navbar-dark px-2">
+        <div className="container-fluid d-flex align-items-center justify-content-between">
+          <a className="navbar-brand px-4" href="/">
+            <img src={Logo} alt="The Letter J" width="40" height="40" data-custom-cursor/>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ms-auto mb-2 px-2 mb-md-0">
+              <li className="nav-item px-2">
+                <a
+                  className="nav-link"
+                  aria-current="page"
+                  href="https://drive.google.com/file/d/1lomHyh0YdDq-agguoIkI6t9Fo1NCVH_0/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  data-custom-cursor
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      
     );
 }
 
