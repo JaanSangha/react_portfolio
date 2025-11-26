ef6c9b116e29431688a44c21b8dc291f041a1959
jaan <jaansangha10@gmail.com>
2025-11-26 11:42:28 -0500

    added real estate site to projects

--- src/websites.js
+++ src/websites.js
@@ -2,19 +2,20 @@ import RSThumb from "./components/img/annaThumbedit.png"
 import SFThumb from "./components/img/simayathumb.png"
 import SBThumb from "./components/img/sanghabrosthumb.jpg"
 import WWThumb from "./components/img/WWThumb.png"
+import YRHousesThumb from "./components/img/YRHousesThumb.png"
 
 const websites = [
 {
     id: 1,
-    title: "We Liberate the World",
-    thumbnail: WWThumb,
-    link: "https://weliberatetheworld.com/"
+    title: "York Region Houses for Sale",
+    thumbnail: YRHousesThumb,
+    link: "https://www.yorkregionhousesforsale.com/"
 },
 {
     id: 2,
-    title: "Simaya Foundation",
-    thumbnail: SFThumb,
-    link: "https://simayafoundation.org/"
+    title: "We Liberate the World",
+    thumbnail: WWThumb,
+    link: "https://weliberatetheworld.com/"
 },
 {
     id: 3,
@@ -24,6 +25,12 @@ const websites = [
 },
 {
     id: 4,
+    title: "Simaya Foundation",
+    thumbnail: SFThumb,
+    link: "https://simayafoundation.org/"
+},
+{
+    id: 5,
     title: "Rumours of Stevie",
     thumbnail: RSThumb,
     link: "https://rumoursofstevie.com/"
